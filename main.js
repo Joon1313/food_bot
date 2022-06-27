df5c6a822c7efde2a4ea87a0d9357c2c093acdd3
Joon1313 <wkdrud13@gmail.com>
2022-06-27 23:50:07 +0900

    [feat] add img save

--- main.js
+++ main.js
@@ -2,13 +2,34 @@ import axios from "axios";
 import fs from 'fs';
 import { headers } from './config.js'
 
-axios.get('https://i.instagram.com/api/v1/users/web_profile_info/?username=casamia__food', {headers}).then((res) => {
-  const data = res.data.data;
-  const latestPost = data.user.edge_owner_to_timeline_media.edges[0].node
-  const text = latestPost.edge_media_to_caption.edges[0].node.text.split('\n')[0];
-  const imgs = latestPost.edge_sidecar_to_children.edges.map((edge) => edge.node.display_url );
-  console.log('텍스트', text);
-  console.log('이미지', imgs);
+// 인스타 크롤링
+const res = await axios.get('https://i.instagram.com/api/v1/users/web_profile_info/?username=casamia__food', {headers});
+const data = res.data.data;
+const latestPost = data.user.edge_owner_to_timeline_media.edges[0].node
+const text = latestPost.edge_media_to_caption.edges[0].node.text.split('\n')[0];
+const urls = latestPost.edge_sidecar_to_children.edges.map((edge) => edge.node.display_url );
+console.log(text);
+
+// 비동기 제어
+// Promise.all(urls.map(imgFetch))
+// function imgFetch(url){
+//   axios.get(url, {responseType: 'arraybuffer'}).then((res) => {
+//     fs.writeFile(`./imgs/f.jpg`, buffer.data, (err)=>{
+//       console.log(err);
+//     });
+//   });
+// }
+
+// 이미지 파일 write
+urls.forEach(async (url, i) => {
+  const buffer = await axios.get(url, {responseType: 'arraybuffer'});
+  fs.writeFile(`./imgs/f${i}.jpg`, buffer.data, (err)=>{
+    console.log(err);
+  });
 })
 
-console.log(headers);
+
+// 저장된 이미지 파일 read
+fs.readdir('./imgs',(err, fileList)=>{
+  console.log(fileList)
+})
